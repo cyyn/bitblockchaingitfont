b7d95249bf9b1fb9551d144b5fe1549ba9e84fc0
cyyn <2908464947@qq.com>
2019-04-25 11:25:31 +0800

    add blocklist

---
+++ viewmodels/Blocklist.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        blocklist: '',
+        blockheight:'',
+        blocktransaction:'',
+    },
+    mounted () {
+        console.log('view mounted');
+        var url =new URL(location.href);
+        var blockheight=url.searchParams.get("blockheight");
+        this.blockheight=blockheight;
+        this.getblocklist();
+    },
+    methods: {
+        getblocklist() {
+            axios.get('http://localhost:8080/block/getBlockDetailByHeight', {
+                params: {
+                   blockheight:this.blockheight,
+                }
+              })
+              .then(function (response) {
+                console.log(response);
+                app.blocklist=response.data;
+                app.blocktransaction=response.data.transactions;
+              })
+              .catch(function (error) {
+                console.log(error);
+              })
+        }
+    }
+})
