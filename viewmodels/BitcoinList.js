27281637951dae511fabf98ba6c8281b130a8ee5
cyyn <2908464947@qq.com>
2019-04-27 20:26:48 +0800

    update bictionlist

--- viewmodels/BitcoinList.js
+++ viewmodels/BitcoinList.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         input:'',
-        bitcoinblocklist: []
+        bitcoinblocklist: [],
+        activeName:'first',
     },
     computed: {
         showbitcoinblocklist(){
