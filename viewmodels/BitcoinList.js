dcb304534649373b27536edcf21b5491c6e7fa60
cyyn <2908464947@qq.com>
2019-04-29 19:44:44 +0800

    update bitcoinlist,html and bitcoinlist.js

--- viewmodels/BitcoinList.js
+++ viewmodels/BitcoinList.js
@@ -3,21 +3,31 @@ var app = new Vue({
     data: {
         input:'',
         bitcoinblocklist: [],
-        activeName:'first',
+        activeName: 'first',
+        bittransactionlist: [],
+        blockhash: '',
     },
     computed: {
-        showbitcoinblocklist(){
+        showbitcoinblocklist() {
             var now = Date.now();
             this.bitcoinblocklist.forEach(block => {
-                block.showtime=parseInt((now - block.time)/1000/60);
-                block.showsizeOnDisk=block.sizeOnDisk.toLocaleString('en')
+                block.showtime = parseInt((now - block.time) / 1000 / 60);
+                block.showsizeOnDisk = block.sizeOnDisk.toLocaleString('en')
             });
             return this.bitcoinblocklist;
+        },
+        showbitcoinTransactionlist() {
+            var now = Date.now();
+            this.bittransactionlist.forEach(transaction => {
+                transaction.showtime = parseInt((now - transaction.timemiao) / 1000 / 60);
+            });
+            return this.bittransactionlist;
         }
     },
     mounted() {
         console.log("开始执行");
         this.getblocklist();
+        this.gettransactionlist();
     },
     methods: {
         // 展示块信息
@@ -25,11 +35,26 @@ var app = new Vue({
             axios.get('http://localhost:8080/block/getRecentBlocks')
                 .then(function (response) {
                     console.log(response);
-                    app.bitcoinblocklist=response.data;
+                    app.bitcoinblocklist = response.data;
                 })
                 .catch(function (error) {
                     console.log(error);
                 })
-        }
+        },
+        //展示交易信息
+        gettransactionlist() {
+            axios.get('http://localhost:8080/block/getblockhash', {
+                params: {
+                    blockhash: this.blockhash,
+                }
+            })
+                .then(function (response) {
+                    app.bittransactionlist = response.data;
+                    // app.blockhash=response.data(0).blockhash;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        },
     }
 })
